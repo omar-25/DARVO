8eeadd4f60c5b92bcd7aa70b338017ae772191be
omar-25 <omarahmed252612@gmail.com>
2026-05-12 21:16:24 +0300

    minor changes

--- index.js
+++ index.js
@@ -35,12 +35,6 @@ app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
 
 app.get('/', (req, res) => {
     res.send('Server side for Your application is now running....');
