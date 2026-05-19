6505742b7955a7018f15b0f7900cef72a37015ab
KamalMohamed17 <kamalabdelhai05@gmail.com>
2026-05-19 22:30:30 +0300

    Adduser front end & Logut btn created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import Signup from './pages/Signup';
 import PendingProperties from './pages/admin/PendingProperties';
 import CreateProperty from './pages/CClient/createProperty';
 import Homepage from './pages/Client/HomePage';
-import CompareProperties from './pages/Client/CompareProperties';
 import AdvancedFilterPage from './pages/Client/AdvancedFilterPage';
 import PropertyDetails from './pages/Client/PropertyDetails';
 
@@ -17,8 +15,6 @@ function App() {
         <Route path="/admin/properties" element={<PendingProperties />} />
         <Route path="/create-property" element={<CreateProperty />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/compare/:idA/:idB" element={<CompareProperties />} />
         <Route path="/advanced-filter" element={<AdvancedFilterPage />} />
         <Route path="/property/:id" element={<PropertyDetails />} />
       </Routes>
